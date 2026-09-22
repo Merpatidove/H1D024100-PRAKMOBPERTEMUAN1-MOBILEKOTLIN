08bc42082f9a769d02767540f41cc0032c62fa17
merpatidove <merpatidove@users.noreply.github.com>
2026-09-22 21:57:46 +0700

    Pertemuan 3: dynamic lists (LazyRow/LazyVerticalGrid), docs screenshots, gitignore cleanup

---
+++ app/src/main/java/com/pemmob/anakbaik/HomeActivity.kt
@@ -0,0 +1,20 @@
+package com.pemmob.anakbaik
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.pemmob.anakbaik.ui.screen.DaftarProductScreen
+import com.pemmob.anakbaik.ui.theme.JualanTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JualanTheme {
+                DaftarProductScreen()
+            }
+        }
+    }
+}
